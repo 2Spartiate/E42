d14aade17237faf5e0482d4e163e7c01e6ecad9b
2Spartiate <2Spartiate@gmail.com>
2024-09-19 10:23:23 +0200

    Mise a jour jusqu'a C03

---
+++ C03/ex05/ft_strlcat.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gderrien <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 12:08:52 by gderrien          #+#    #+#             */
+/*   Updated: 2024/09/18 12:58:54 by gderrien         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dlen;
+	unsigned int	slen;
+	unsigned int	i;
+
+	dlen = ft_strlen(dest);
+	slen = ft_strlen(src);
+	if (size <= dlen)
+		return (slen + size);
+	i = 0;
+	while (src[i] != '\0' && dlen + i < size - 1)
+	{
+		dest[dlen + i] = src[i];
+		i++;
+	}
+	dest[dlen + i] = '\0';
+	return (dlen + slen);
+}
