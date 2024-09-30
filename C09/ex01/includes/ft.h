383c084822c8c9fe9df9f4137b215c882a850807
2Spartiate <2Spartiate@gmail.com>
2024-09-30 12:03:17 +0200

    Ajout de C09

---
+++ C09/ex01/includes/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gderrien <gaetan.derrien@gmail.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/15 11:46:25 by gderrien          #+#    #+#             */
+/*   Updated: 2024/09/15 12:17:41 by gderrien         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
